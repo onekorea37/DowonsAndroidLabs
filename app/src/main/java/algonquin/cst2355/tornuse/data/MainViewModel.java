4099bf3cc27870282eb9045fe8dc8571cc159b20
Dowon <onekorea37@gmail.com>
2023-05-22 11:01:51 -0400

    Finished week 2

--- app/src/main/java/algonquin/cst2355/tornuse/data/MainViewModel.java
+++ app/src/main/java/algonquin/cst2355/tornuse/data/MainViewModel.java
@@ -7,4 +7,6 @@ public class MainViewModel extends ViewModel {
 
     public MutableLiveData<String> editString = new MutableLiveData<>();
 
+    public MutableLiveData<Boolean> isSelected = new MutableLiveData<>();
+
 }
--- app/src/main/java/algonquin/cst2355/tornuse/ui/MainActivity.java
+++ app/src/main/java/algonquin/cst2355/tornuse/ui/MainActivity.java
@@ -6,8 +6,12 @@ import androidx.lifecycle.ViewModelProvider;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CheckBox;
 import android.widget.EditText;
+import android.widget.RadioButton;
+import android.widget.Switch;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import algonquin.cst2355.tornuse.data.MainViewModel;
 import algonquin.cst2355.tornuse.databinding.ActivityMainBinding;
@@ -26,6 +30,7 @@ public class MainActivity extends AppCompatActivity {
         variableBinding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView( variableBinding.getRoot() ); // loads XML on screen
 
+        // part 1
         variableBinding.mybutton.setOnClickListener(click -> {
             model.editString.postValue(variableBinding.myedittext.getText().toString());
         });
@@ -34,6 +39,38 @@ public class MainActivity extends AppCompatActivity {
             variableBinding.textview.setText("Your edit text has " + s);
         });
 
+
+        // part 2 - checkbox, switch, radiobutton
+        model.isSelected.observe(this, selected -> {
+           variableBinding.checkbox.setChecked(selected);
+           variableBinding.radiobutton.setChecked(selected);
+           variableBinding.switchWidget.setChecked(selected);
+
+           // Show Toast Message
+            String message = "The value is now: " + selected;
+            Toast.makeText(this, message, Toast.LENGTH_SHORT).show();
+        });
+
+        variableBinding.checkbox.setOnCheckedChangeListener( (btn, selected) -> {
+            model.isSelected.postValue(selected);
+        });
+
+        variableBinding.switchWidget.setOnCheckedChangeListener( (btn, selected) -> {
+            model.isSelected.postValue(selected);
+        });
+
+        variableBinding.radiobutton.setOnCheckedChangeListener( (btn, selected) -> {
+            model.isSelected.postValue(selected);
+        });
+
+
+        // Last part - image
+        variableBinding.myimagebutton.setOnClickListener(click -> {
+            int width = click.getWidth();
+            int height = click.getHeight();
+            String message = "The width = " + width + " and height = " + height;
+            Toast.makeText(MainActivity.this, message, Toast.LENGTH_SHORT).show();
+        });
 
     }
 }
