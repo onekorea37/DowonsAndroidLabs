045254f096e5293f13922cc9701d5f85a9e1bd4d
Dowon <onekorea37@gmail.com>
2023-06-05 16:48:19 -0400

    Week4 - save file 1

--- app/src/main/java/algonquin/cst2355/tornuse/MainActivity.java
+++ app/src/main/java/algonquin/cst2355/tornuse/MainActivity.java
@@ -2,16 +2,57 @@ package algonquin.cst2355.tornuse;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    Button LoginButton;
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.w("MainActivity", "The application is now visible on screen");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.w("MainActivity", "The application is now responding to user input");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.w("MainActivity", "The application no longer responds to user input");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.w("MainActivity", "The application is no longer visible");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.w("MainActivity", "Any memory used by the application is freed.");
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        Log.w("MainActivity", "In onCreate() - Loading widgets");
 
+        LoginButton = findViewById(R.id.LoginButton);
+        LoginButton.setOnClickListener( click -> {
+            Intent nextPage = new Intent(MainActivity.this, SecondActivity.class);
+            startActivity(nextPage);
+        });
 
     }
 }
